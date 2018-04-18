55036588fb2e22e19eb12b37b9b5aea53c65e038
Jeferson Vieira <jeff_malganis@hotmail.com>
2018-04-18 19:09:47 -0300

    add: required wallet_central methods

--- lib/wallet_central.rb
+++ lib/wallet_central.rb
@@ -1,2 +1,13 @@
+require_relative 'csv_parse'
+
+CsvParse.new
+
 class WalletCentral
+  def self.transfer(from, to, currency, amount)
+    #todo
+  end
+
+  def self.output(filter = nil)
+    #todo
+  end
 end
