735a69cc8935723330f42c10c7aee5102159531f
Jeferson Vieira <jeff_malganis@hotmail.com>
2018-04-18 19:23:06 -0300

    add: client get_wallet specs

--- spec/lib/client_spec.rb
+++ spec/lib/client_spec.rb
@@ -20,4 +20,21 @@ RSpec.describe Client, type: :model do
       end
     end
   end
+
+  describe ".get_currency_wallet" do
+    let!(:client) { Client.create(name: "jeff") }
+    let!(:wallet) { Wallet.create(currency: "USD", amount: 50.0, client_id: client.id) }
+
+    context "when client have the Wallet" do
+      it "should return a Wallet" do
+        expect( Client.get_currency_wallet("jeff", "USD") ).to eq(wallet)
+      end
+    end
+
+    context "when client doesnt have the Wallet" do
+      it "should return Exception" do
+        expect { Client.get_currency_wallet("jeff","BRL") }.to raise_error "No wallet whit currency BRL found for jeff"
+      end
+    end
+  end
 end
