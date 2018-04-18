9667ce1571c0192eae45c19036dd289681ac955a
Jeferson Vieira <jeff_malganis@hotmail.com>
2018-04-18 19:26:46 -0300

    add: client get_wallet method

--- lib/client.rb
+++ lib/client.rb
@@ -7,4 +7,14 @@ class Client < Sequel::Model
     client
   end
 
+  def self.get_currency_wallet(client_name, currency, return_first = false)
+    client = Client.find(name: client_name)
+    raise Exception.new("No client #{client_name} found") if client.nil?
+
+    wallet = client.wallets.find{|w| w.currency == currency}
+    raise Exception.new("No wallet whit currency #{currency} found for #{client_name}") if wallet.nil? && !return_first
+    wallet = client.wallets.first if wallet.nil? && return_first
+    wallet
+  end
+
 end
