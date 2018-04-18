9c85babd2834088a7c16a232d58275ca4c636c5f
Jeferson Vieira <jeff_malganis@hotmail.com>
2018-04-18 19:30:05 -0300

    add: wallet withdraw, credit methods

--- lib/wallet.rb
+++ lib/wallet.rb
@@ -1,3 +1,14 @@
 class Wallet < Sequel::Model
   many_to_one :client
+
+  def withdraw(withdraw_amount)
+    raise Exception.new("wallet dont have minimun amount") if self.amount < withdraw_amount
+    self.amount -= withdraw_amount
+    self.save
+  end
+
+  def credit(credit_amount, credit_currency)
+    self.amount += credit_amount
+    self.save
+  end
 end
--- spec/lib/wallet_spec.rb
+++ spec/lib/wallet_spec.rb
@@ -8,11 +8,14 @@ RSpec.describe Wallet, type: :model do
   describe ".withdraw" do
     context "when wallet have the amount" do
       it "should decrease the value" do
+        wallet.withdraw(50.0)
+        expect(wallet.amount).to eq(50.0)
       end
     end
 
     context "when wallet doesnt have the amount" do
       it "should return a exception" do
+        expect{ wallet.withdraw(150) }.to raise_error "wallet dont have minimun amount"
       end
     end
   end
@@ -20,11 +23,15 @@ RSpec.describe Wallet, type: :model do
   describe ".credit" do
     context "when wallet have the same currency" do
       it "should sum the value" do
+        wallet.credit(50.0, "USD")
+        expect(wallet.amount).to eq(150.0)
       end
     end
 
     context "when wallet doesnt have same currency" do
       it "should exchange and sum the value" do
+        wallet.credit(50.0, "EUR")
+        expect(wallet.amount).to eq(150.0)
       end
     end
   end
